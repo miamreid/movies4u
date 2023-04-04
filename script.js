ac5dae3b3ac9977a0662a15afe464561199ebe46
Jose <josejrod07@gmail.com>
2023-04-03 20:34:35 -0400

    introduced OMDB API

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //oxAVFGzkee0zA6fTb7Q4lA32umzPGXjC
 
+/*
 function getNYTAPI() {
     var requestUrl = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=twilight&api-key=oxAVFGzkee0zA6fTb7Q4lA32umzPGXjC";
 
@@ -29,3 +30,49 @@ function getdatabaseAPI() {
 }
 
 getdatabaseAPI();
+
+
+
+
+
+
+$(document.readyState(function(){
+  $('recommendBtn').click(omdbAPI);
+}))
+
+*/
+
+
+function omdbAPI(){
+  var apiKey = 'def92d1a'
+  var openMovieUrl = `https://www.omdbapi.com/?apikey=${apiKey}&type=movie`; 
+  
+  fetch(openMovieUrl)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(data){
+      console.log(data);
+      var randomIndex = Math.floor(Math.random() * data.Search.length);
+      var imdbID = data.Search[randomIndex].imdbID;
+      
+      return fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}&plot=full`);
+    })
+    .then(response => response.json())
+    .then(function(movie){
+
+      var title = movie.Title;
+      var genre = movie.Genre;
+      var year = movie.Year;
+      var plot = movie.Plot;
+
+      console.log(title);
+      console.log(genre);
+      console.log(year);
+      console.log(plot);
+
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
