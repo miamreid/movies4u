c9f444eb71dd6466788dcf5f54f752006e9406ed
Jose <josejrod07@gmail.com>
2023-04-04 18:37:22 -0400

    added URL builder

--- script.js
+++ script.js
@@ -16,44 +16,46 @@ function getNYTAPI() {
 
 getNYTAPI();
 
-function getdatabaseAPI() {
-    var requestUrl = "https://api.themoviedb.org/3/movie/550?api_key=701ebb9db6b0f6cd175ada217a8261bb";
-
-    fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-
-    })
-}
-
-getdatabaseAPI();
-
-
-
-
-
-
 $(document.readyState(function(){
   $('recommendBtn').click(omdbAPI);
 }))
 
+
 */
 
-
 function moviedbAPI(){
-  var apiKey = '701ebb9db6b0f6cd175ada217a8261bb'
-  var movieUrl = `https://api.themoviedb.org/3/movie/550/recommendations?api_key=701ebb9db6b0f6cd175ada217a8261bb&language=en-US&page=1`; 
+  var API_KEY = "701ebb9db6b0f6cd175ada217a8261bb";
+  var API_URL = "https://api.themoviedb.org/3/discover/movie";
   
-  fetch(movieUrl)
+  // Get the filter inputs
+  var releaseYear = 2008;
+  var genre = 'action';
+  
+  // Construct the API URL with the filter parameters
+  var url = `${API_URL}?api_key=${API_KEY}&primary_release_year=${releaseYear}&with_genres=${genre}&sort_by=vote_average.desc`;
+
+  // Fetch the data from the API
+  fetch(url)
     .then(function(response){
-      return response.json();
+      return response.json()
     })
-    .then(function(data){
-      console.log(data);
-    })
+    .then(function(data) {
+  // Display the first movie that matches the filter criteria
+    if (data.results.length > 0) {
+      var movie = data.results[0];
+      var title = movie.title;
+      var releaseDate = movie.release_date;
+      var genre = movie.genre_ids[0];
+
+      console.log(`Title: ${title}, Release Date: ${releaseDate}, Genre: ${genre}`);
+      } else {
+      console.log("No movies found that match the filter criteria.");
+      }
+    });
   }
 
 moviedbAPI();
+
+
+
+  
