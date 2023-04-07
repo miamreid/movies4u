9e776a05371fd858f16e4b2865a1ff7115c08b69
miamreid-pNUkWI <miamreid@gmail.com>
2023-04-06 21:37:04 -0400

    local storage stuff

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -123,7 +123,7 @@ function moviedbAPI(){
   // Construct the API URL with the filter parameters
   lengthTime = userLength;
 
-  var discoverURL = `${API_URL}?api_key=${API_KEY}&top_rated&primary_release_year=${releaseYear}&with_genres=${genre}&with_runtime.gte=${lengthTime}&include_adult=true&append_to_response=genre&sort_by=popularity.desc&language=en-US&page=1`;
+  var discoverURL = `${API_URL}?api_key=${API_KEY}&top_rated&primary_release_year=${releaseYear}&with_genres=${genre}&with_runtime.gte=${lengthTime}&include_adult=false&sort_by=popularity.desc&language=en-US&watch_region=north%20america&page=1`;
 
   // Fetch the data from the API
   fetch(discoverURL)
@@ -139,61 +139,20 @@ function moviedbAPI(){
       for (var i = 0; i < 19; i++){
         var result = document.getElementById("img-" + [i]);
         result.src = "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
+        var storeImagePath = "Stored Image Path-" + [i];
+        var imagePath = data.results[i].poster_path;
+        localStorage.setItem(storeImagePath, imagePath);
+
         var title = document.getElementById("title-" + [i]);
         title.innerHTML = data.results[i].title;
-        var storedMovieId = "MovieID-" + [i];
-        var movieId = data.results[i].id;
-        localStorage.setItem (storedMovieId, movieId);
-      }
-/*
-      var storeMovieId1 = "MovieID-1";
-      var movieId1 = data.results[0].id;
-      localStorage.setItem(storeMovieId1, movieId1);
+        var storeTitle = "Stored Title-" + [i];
+        localStorage.setItem(storeTitle, title.innerHTML);
 
-     
-      var result1 = document.getElementById("img-1");
-      result1.src = "https://image.tmdb.org/t/p/w500" + data.results[0].poster_path;
-      var title1=document.getElementById("title-1");
-      var result2 = document.getElementById("img-2");
-      result2.src = "https://image.tmdb.org/t/p/w500" + data.results[1].poster_path;
-      var result3 = document.getElementById("img-3");
-      result3.src = "https://image.tmdb.org/t/p/w500" + data.results[2].poster_path;
-      var result4 = document.getElementById("img-4");
-      result4.src = "https://image.tmdb.org/t/p/w500" + data.results[3].poster_path;
-      var result5 = document.getElementById("img-5");
-      result5.src = "https://image.tmdb.org/t/p/w500" + data.results[4].poster_path;
-      var result6 = document.getElementById("img-6");
-      result6.src = "https://image.tmdb.org/t/p/w500" + data.results[5].poster_path;
-      var result7 = document.getElementById("img-7");
-      result7.src = "https://image.tmdb.org/t/p/w500" + data.results[6].poster_path;
-      var result8 = document.getElementById("img-8");
-      result8.src = "https://image.tmdb.org/t/p/w500" + data.results[7].poster_path;
-      var result9 = document.getElementById("img-9");
-      result9.src = "https://image.tmdb.org/t/p/w500" + data.results[8].poster_path;
-      var result10 = document.getElementById("img-10");
-      result10.src = "https://image.tmdb.org/t/p/w500" + data.results[9].poster_path;
-      var result11 = document.getElementById("img-11");
-      result11.src = "https://image.tmdb.org/t/p/w500" + data.results[10].poster_path;
-      var result11 = document.getElementById("img-12");
-      result11.src = "https://image.tmdb.org/t/p/w500" + data.results[11].poster_path;
-      var result13 = document.getElementById("img-13");
-      result13.src = "https://image.tmdb.org/t/p/w500" + data.results[12].poster_path;
-      var result14 = document.getElementById("img-14");
-      result14.src = "https://image.tmdb.org/t/p/w500" + data.results[13].poster_path;
-      var result15 = document.getElementById("img-15");
-      result15.src = "https://image.tmdb.org/t/p/w500" + data.results[14].poster_path;
-      var result16 = document.getElementById("img-16");
-      result16.src = "https://image.tmdb.org/t/p/w500" + data.results[15].poster_path;
-      var result17 = document.getElementById("img-17");
-      result17.src = "https://image.tmdb.org/t/p/w500" + data.results[16].poster_path;
-      var result18 = document.getElementById("img-18");
-      result18.src = "https://image.tmdb.org/t/p/w500" + data.results[17].poster_path;
-      var result19 = document.getElementById("img-19");
-      result19.src = "https://image.tmdb.org/t/p/w500" + data.results[18].poster_path;
-      var result20 = document.getElementById("img-20");
-      result20.src = "https://image.tmdb.org/t/p/w500" + data.results[19].poster_path;
-      */
-      
+        var plot = document.getElementById("plot-" + [i]);
+        plot.innerHTML = data.results[i].overview;
+        var storePlot = "Stored Plot-" + [i];
+        localStorage.setItem(storePlot, plot.innerHTML)
+      }
   })
   return window.location.assign("./results.html");
 }
