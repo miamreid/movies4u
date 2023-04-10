b0c50bd71a72f2d287c0b71ac27f0e39ebf3f206
Jose <josejrod07@gmail.com>
2023-04-10 14:37:25 -0400

    added New York Times API

--- Assets/js/movie-page.js
+++ Assets/js/movie-page.js
@@ -1,41 +1,13 @@
-/* This is for the results.js
-function infoRedirect() {
-  var cards = document.querySelectorAll('.result-item');
-  cards.forEach(function(card) {
-    var index = card.getAttribute('data-index');
-    card.addEventListener('click', function(){
-
-      var storedMovieTitle = localStorage.getItem('Stored Title-' + index);
-      var titleSelectKey = "Selected Title";
-      localStorage.setItem(titleSelectKey, storedMovieTitle);
-
-      var storedImgPath = localStorage.getItem("Stored Image Path-" + index);
-      var imgSelectKey = "Selected Image";
-      localStorage.setItem(imgSelectKey, storedImgPath);
-
-      var storedPlot = localStorage.getItem("Stored Plot-" + index);
-      var plotSelectKey = "Selected Plot"
-      localStorage.setItem(plotSelectKey, storedPlot);
-
-      toMoviePage();
-    });
-  }); 
-}
-
-function toMoviePage(){
-  window.location.assign('./movie-page.html');
-}
-  */
-
+// Local Storage Elements For Selected Movie
 var selectedImg = localStorage.getItem("Selected Image");
 var selectedTitle = localStorage.getItem("Selected Title");
 var selectedGenre = localStorage.getItem("Genre Text");
 var selectedPlot = localStorage.getItem("Selected Plot");
 
+// Function to Display Selected Information
 function selectedInfo(){
     var printImg = document.getElementById("selected-img");
     printImg.src = "https://image.tmdb.org/t/p/w500" + selectedImg;
-
     
     var printTitle = document.getElementById("selected-title");
     printTitle.textContent = selectedTitle;
@@ -50,14 +22,41 @@ function selectedInfo(){
 selectedInfo();
 
 function getNYTAPI(){
+  // Fetch Request for New York Times API
     var keyNYT = "oxAVFGzkee0zA6fTb7Q4lA32umzPGXjC"
-    var reviewUrl = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${selectedTitle}&api-key=${keyNYT}`;
+    var movieUrl = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${selectedTitle}&order=by-opening-date&api-key=${keyNYT}`;
     
-    fetch(reviewUrl)
+    fetch(movieUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
+
       console.log(data)
+      
+      // Displays Movie's MPAA Rating
+      var rating = data.results[0].mpaa_rating;
+      var printRating = document.getElementById("rating");
+      printRating.textContent = rating;
+      
+      // Displays Movie Review Headline and URL Link
+      var reviewHead = data.results[0].headline;
+      var reviewUrl = data.results[0].link.url;
+      var linkText = "Read the New York Times Review For This Movie";
+      var printHead = document.getElementById("review-headline");
+      printHead.textContent = reviewHead;
+      var linkTag = document.getElementById("link-tag");
+      linkTag.textContent = linkText;
+      linkTag.setAttribute("href", reviewUrl);
+
+      // Displays the Movie Reviewer's Name
+      var reviewerName = data.results[0].byline;
+      var printReviewerName = document.getElementById("byline");
+      printReviewerName.textContent = "Reviewer Name: " + reviewerName;
     })
 }
+
+getNYTAPI();
+
+
+
